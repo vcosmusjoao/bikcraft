41ab3e8d322b1fe70e2a969fdc6b7bbca24ee7f0
vcosmusjoao <rm88372@fiap.com.br>
2021-06-30 16:27:49 -0300

    Paginas JavaScript Adicionadas e implementação de plugins javascript

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+if(window.SimpleSlide){
+new SimpleSlide({
+slide: "quote", // nome do atributo data-slide="principal"
+  time: 4000 // tempo de transição dos slides 5000 é 5 segundos
+});
+
+
+new SimpleSlide({
+  slide: "portfolio", // nome do atributo data-slide="principal"
+    time: 4000, // tempo de transição dos slides 5000 é 5 segundos
+    nav:true
+  });
+}
+
+if(window.SimpleAnime){
+new SimpleAnime({
+
+
+
+});
+
+}
